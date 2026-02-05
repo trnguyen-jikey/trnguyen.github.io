3bd7e2b1d94e366a33f7f1647cb992650ae00bd8
Kru_alien <dangkhoa0999u@gmail.com>
2026-02-05 17:22:49 +0700

    Update discord.js

--- assets/js/discord.js
+++ assets/js/discord.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userId = "875062909498175558"; //replace your discord id here
+    const userId = "1441770577353244772"; //replace your discord id here
     const apiUrl = `https://discord-lookup-api-alpha.vercel.app/v1/user/${userId}`;
 
     const avatarFrame = document.getElementById('avatar-frame');
